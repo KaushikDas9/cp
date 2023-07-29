6363bfe5845a3dd16040a1ab36110ee1d2640675
KaushikDas9 <dasbabai2017@gmail.com>
2023-07-29 18:52:30 +0530

    sum.cpp

---
+++ Anton_and_Polyhedrons.java
@@ -0,0 +1,26 @@
+//package Striver_CP_SHEET;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Anton_and_Polyhedrons {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        HashMap<String,Integer> store = new HashMap<String,Integer>();
+        store.put("Tetrahedron",4);
+        store.put("Cube",6);
+        store.put("Octahedron",8);
+        store.put("Dodecahedron",12);
+        store.put("Icosahedron",20);
+        int ans=0;
+        for(int i = 0; i < n; i++)
+        {
+            String temp = scan.next();
+            ans+=store.get(temp);
+        }
+        System.out.println(ans);
+
+    }
+
+}
--- The_New_Year_Meeting_Friends.java
+++ The_New_Year_Meeting_Friends.java
@@ -1,2 +1,18 @@
-package Striver_CP_SHEET;public class The_New_Year_Meeting_Friends {
+package Striver_CP_SHEET;
+import java.util.Scanner;
+public class The_New_Year_Meeting_Friends {
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+        int x1 = scan.nextInt();
+        int x2 = scan.nextInt();
+        int x3 = scan.nextInt();
+
+        // Logic
+        // (x2-x1) + (x3-x2) = total minimum distance
+        // =) x2-x1+x3-x2 = total maximum distance
+        // =) x3 - x1 = total minimum distance (here x3 is max distance ans x1 is min distance)
+
+        System.out.println((Math.max(x1,Math.max(x2,x3)))-(Math.min(x1,Math.min(x2,x3))));
+    }
 }
