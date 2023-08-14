6b0169f592a4c9df104d63e6879a5fcf75fe3dd3
KaushikDas9 <dasbabai2017@gmail.com>
2023-08-14 16:16:10 +0530

    Erasing_Zeroes.java

---
+++ Erasing_Zeroes.java
@@ -0,0 +1,28 @@
+//package Striver_CP_SHEET;
+
+import java.util.Scanner;
+
+public class Erasing_Zeroes {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        int take = scan.nextInt();
+        while (take-- > 0) {
+
+        String str = scan.next();
+            int pre = -1;
+            int ans=0;
+            for(int i = 0; i<str.length();i++) {
+                if(str.charAt(i)=='1') {
+                    if(pre!=-1) ans += i-pre-1;
+                     pre = i;
+
+                }
+
+            }
+            System.out.println(ans);
+        }
+    }
+}
